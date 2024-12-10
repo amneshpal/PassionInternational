034e0abc0b2717c272d66df290547d63357a8df8
amneshpal <amneshpal016@gmail.com>
2024-12-10 14:17:59 +0530

    add path

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'    https://passioninternational.onrender.com',
+
     credentials:true
 }
 
